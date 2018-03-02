706f912f20b3907bf2e9b32546403f3711666093
Parth Parth <hiparthparth@gmail.com>
2018-03-02 10:13:40 +0530

    Create Replace with alphabet position.js

---
+++ Replace with alphabet position.js
@@ -0,0 +1,6 @@
+function alphabetPosition(text) {
+  return text.toLowerCase().split('')
+        .filter( c => c >= 'a' & c <= 'z' )
+        .map( c => c.charCodeAt(0) - 'a'.charCodeAt(0) + 1)
+        .join(' ');
+}
